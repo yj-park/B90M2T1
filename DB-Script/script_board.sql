626bd0803a52278bf6531da39c327e219f140f01
Park <sccv7777@hanmail.net>
2017-03-06 15:13:20 +0900

    DB-Script / script_board.sql  게시판과 연관된 테이블 올렸습니다.

---
+++ DB-Script/script_board.sql
@@ -0,0 +1,38 @@
+============= 게시판 테이블 =====================================
+create table tb_board (
+  no number(10) primary key,
+  member_id varchar2(50) not null,
+  title varchar2(100) not null,
+  content varchar2(3000) not null,
+  view_cnt number(10) default 0,
+  reg_date date default sysdate not null
+);
+
+desc tb_board;
+
+create sequence s_board_no;
+
+insert into tb_board (no, member_id, title, content)
+values(s_board_no.nextVal, '티모', '안녕하세요', '티모입니다');
+
+select * from tb_board;
+
+===========================================================
+
+============= 파일 테이블 =====================================
+
+create table tb_file (
+
+  NO NUMBER(10) PRIMARY KEY,
+  SYSTEM_NAME VARCHAR2(50) NOT NULL,
+  ORI_NAME VARCHAR(50) NOT NULL,
+  SAVE_PATH VARCHAR(50) NOT NULL,
+  BOARD_NO NUMBER(10) NOT NULL
+
+);
+
+CREATE SEQUENCE S_FILE_NO;
+
+DESC TB_FILE;
+
+SELECT * FROM TB_FILE;
